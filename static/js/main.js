69b687e5b37dd7d27a33771306c6185f6429c124
nancywong <nancypwong@gmail.com>
2015-09-06 05:02:08 -0400

    Style new camera

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,28 @@
+Webcam.set({
+        width: 320,
+        height: 240,
+        flip_horiz: true,
+    });
+Webcam.attach( '#my_camera' );
+
+function take_snapshot() {
+    Webcam.snap( function(data_uri) {
+        // snap complete, image data is in 'data_uri'
+        Webcam.upload( data_uri, '/image_upload', function(code, text) {
+            // Upload complete!
+            // 'code' will be the HTTP response code from the server, e.g. 200
+            // 'text' will be the raw response content
+            console.log(text);
+        } );
+
+    });
+}
+Webcam.on("live", function() {
+
+window.setInterval(function(){
+    take_snapshot();
+}, 1000);
+});
 
 function scrollBottom(){
     $("html, body").animate({ scrollTop: $(document).height() }, "fast");
